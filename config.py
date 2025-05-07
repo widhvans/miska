4c766f6ec9e7d627e03440f04966a42421b34268
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-08 00:16:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"  # Replace with your Telegram bot token
 MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B"  # Hugging Face model
-API_HOST = "138.199.192.141"
+API_HOST = "https://miska-akash5213s-projects.vercel.app/"
 API_PORT = 8000
