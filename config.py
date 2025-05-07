f6cc5341d8aac9c57673e4d37bb2a29113f2dc30
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-08 00:13:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"  # Replace with your Telegram bot token
-MODEL_NAME = "google/gemma-2-2b-it"  # Hugging Face model
+MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B"  # Hugging Face model
 API_HOST = "138.199.192.141"
 API_PORT = 8000
